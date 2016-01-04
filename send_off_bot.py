7fc09cb82470d448c287c94eca26c4e39c93d004
jwlehman <jeremylehman9@gmail.com>
2016-01-04 17:23:10 -0500

    started implementing get_seniors method

--- send_off_bot.py
+++ send_off_bot.py
@@ -1,9 +1,23 @@
 import Bot
 import sys
 
+
 class SendOffBot(Bot):
 
     def __init__(self, name, access_token, callback_url='', avatar_url=''):
-        Bot.__init__(self,name,access_token,callback_url,avatar_url)
-        self.input_file = sys.argv[1]
+        Bot.__init__(self, name, access_token, callback_url, avatar_url)
+        self.input_file = open(sys.argv[1])
+
+    def get_seniors(self, input_file):
+        line = input_file.readline()
+        senior_names = line.split(',')
+        for i in senior_names:
+            i.trim()
+        users = self.get_users(self, self.group_id)
+        name_id = {}
+        for i in users:
+            if i['name'] in senior_names:
+
+
+
 
